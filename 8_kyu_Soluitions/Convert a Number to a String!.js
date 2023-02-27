5b8341e5797da5460fcfc051bb809e0052564f02
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-02-27 12:43:11 +0700

    added Convert a Number to a String!

---
+++ 8_kyu_Soluitions/Convert a Number to a String!.js
@@ -0,0 +1,19 @@
+/*Convert a Number to a String!
+
+DESCRIPTION:
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"*/
+
+
+// simply use .toString() method. nothing scientific
+function numberToString(num) {
+  // Return a string of the number here!
+    let text = num.toString();
+    return text
+}
