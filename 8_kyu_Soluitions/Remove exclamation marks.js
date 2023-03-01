659de742e4f67fbbd311d9991d2ec42e6c673525
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-03-01 15:52:11 +0700

    Remove exclamation marks

---
+++ 8_kyu_Soluitions/Remove exclamation marks.js
@@ -0,0 +1,7 @@
+/* Remove exclamation marks
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.*/
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/gi, ''); // replace ! everywhere
+}
