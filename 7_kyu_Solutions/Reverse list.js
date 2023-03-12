a34ed25fd4efbcf495077a27fe0b52ef1a12c650
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-03-12 11:51:59 +0700

    Reverse list

---
+++ 7_kyu_Solutions/Reverse list.js
@@ -0,0 +1,13 @@
+/* Reverse list
+DESCRIPTION:
+Write reverseList function that simply reverses lists.
+*/
+//JS
+function reverseList(arr) {
+    return arr.reverse(); // use reverse method
+}
+
+/*Python:
+    def reverse_list(lst):
+        return list(reversed(lst))
+*/
