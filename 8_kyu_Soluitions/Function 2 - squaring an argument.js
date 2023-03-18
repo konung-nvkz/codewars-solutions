4f0b39527c6bbe25af52f1e48ff0133ae2792726
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-03-18 09:21:48 +0700

    Function 2 - squaring an argument

---
+++ 8_kyu_Soluitions/Function 2 - squaring an argument.js
@@ -0,0 +1,7 @@
+/*Function 2 - squaring an argument
+DESCRIPTION:
+Now you have to write a function that takes an argument and returns the square of it.*/
+
+function square(value){     
+    return value * value; // operation of squaring itself
+  }
