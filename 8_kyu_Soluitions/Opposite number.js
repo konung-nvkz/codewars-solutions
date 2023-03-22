8556451c05593518eaed2818dd3587336eff96d1
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-03-22 14:49:41 +0700

    Opposite number

---
+++ 8_kyu_Soluitions/Opposite number.js
@@ -0,0 +1,31 @@
+/* Opposite number
+Description
+
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+
+// JS syntax
+function opposite(number) {
+    //your code here
+    return +number*-1 // make the variable a number and change the sign of it
+}
+
+//TS syntax
+/*export class Kata {
+    static opposite(n: number) {
+      // your code here
+      return n * -1;
+    }
+}*/
+
+
+//Python syntax
+/*def opposite(number):
+    return number*(-1)*/
