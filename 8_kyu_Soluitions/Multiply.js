474cf1232e617f587d782be6864639aec8637f3f
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-03-22 14:52:11 +0700

    Multiply

---
+++ 8_kyu_Soluitions/Multiply.js
@@ -0,0 +1,18 @@
+/*Multiply
+DESCRIPTION:
+This code does not execute properly. Try to figure out why.
+*/
+
+//Python:
+/*def multiply(a, b):
+    result = a * b
+    return result*/
+//JavaScript:
+function multiply(a, b){
+  return a * b;
+}
+//TypeScript:
+/*
+export function multiply(a: number, b: number): number {
+  return a * b; // simply change + to * to get a needed result
+}*/
