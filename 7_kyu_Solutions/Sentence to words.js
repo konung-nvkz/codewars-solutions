2565fb3af2198f970be2559a3030b1395743d169
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-04-04 10:11:29 +0700

    sentence to words

---
+++ 7_kyu_Solutions/Sentence to words.js
@@ -0,0 +1,9 @@
+/*Sentence to words
+Write function splitSentence which will create a list of strings from a string.
+
+Example:
+"hello world" -> ["hello", "world"]
+*/
+function splitSentence(s) {
+    return s.split(' ');// don't forget to put " " in .split as a separator 
+}
