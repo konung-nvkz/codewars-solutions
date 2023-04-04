02b4821e1829bbd99066a448f99e83b8631b3577
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-04-04 12:00:03 +0700

    return to sanity

---
+++ 8_kyu_Soluitions/Return to Sanity.js
@@ -0,0 +1,11 @@
+/*Return to Sanity
+DESCRIPTION:
+This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+
+function mystery(sanity) {
+    var results = {
+        sanity: 'Hello'
+    };
+    return results;
+    }
