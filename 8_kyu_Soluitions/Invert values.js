2b08b0568d707e462a919fafadb5b50063ff8e4b
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-04-04 11:48:39 +0700

    invert values

---
+++ 8_kyu_Soluitions/Invert values.js
@@ -0,0 +1,16 @@
+/*Invert values
+DESCRIPTION:
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+*/
+function invert(array) {
+    let newArray = [];    
+    for (i = 0; i < array.length; i++) {
+        newArray.push(-1*array[i]);
+    }
+    return newArray;
+}
