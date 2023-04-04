984828b632c2ffb819c1e42119ce5bf7962575c3
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-04-04 11:36:26 +0700

    reversed

---
+++ 8_kyu_Soluitions/Reversed Words.js
@@ -0,0 +1,17 @@
+/*Reversed Words
+DESCRIPTION:
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+*/
+function reverseWords(str){
+    return str.split(' ').reverse().join(' '); 
+    // we first use split method to create an array
+    // then we reverse it, making the first element the last and so on
+    //finally, we create a new string
+}
