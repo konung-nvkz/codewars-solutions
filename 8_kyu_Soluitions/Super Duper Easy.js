8f8b430db1f05abddcfafc245cc0f113a64e2f10
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-04-05 15:13:40 +0700

    super duper easy

---
+++ 8_kyu_Soluitions/Super Duper Easy.js
@@ -0,0 +1,23 @@
+/*Super Duper Easy
+DESCRIPTION:
+Make a function that returns the value multiplied by 50 and increased by 6.
+If the value entered is a string it should return "Error".
+*/
+function problem(val) {
+    if (typeof(val)=='string') return "Error"; // check the type of the input
+    return val*50+6; // for numbers return what was asked
+}
+
+console.log(problem(5)); // 256
+console.log(problem("hello")); // "error"
+
+
+//And here's the Python code:
+
+def problem(value):
+    if isinstance(value, str):
+        return "Error"
+    return value * 50 + 6;
+
+print(problem(5)) # 256
+print(problem("hello")) # "error
