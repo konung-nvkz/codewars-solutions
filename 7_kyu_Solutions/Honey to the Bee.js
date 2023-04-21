dafb5fe3bddcbcadba4e791c94babd550f0ed211
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-04-21 09:48:36 +0700

    honey

---
+++ 7_kyu_Solutions/Honey to the Bee.js
@@ -0,0 +1,24 @@
+/*Honey to the Bee
+Find out why the amount of honey in the hive is not increasing. (debugging)
+*/
+function Bee(capacity, hive) {
+    this.capacity = capacity;
+    this.hive = hive;
+}
+
+function Hive() {
+this.pollen = 100;
+}
+
+//TODO: The amount of honey in the hive is not increasing.
+Hive.prototype.addPollen = function(pollen) {
+    this.pollen += pollen;
+}
+
+Hive.prototype.getPollen = function() {
+    return this.pollen;
+}
+
+Bee.prototype.unloadPollen = function() {
+    this.hive.addPollen(this.capacity); // need to add up .this
+}
