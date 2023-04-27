68719e8fe3cd20ae5b61d88af4d568a64f5b9d5c
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-04-27 14:42:58 +0700

    hello happy coder

---
+++ 8_kyu_Soluitions/Hello Happy Codevarrior!.js
@@ -0,0 +1,23 @@
+/*Hello Happy Codevarrior!
+VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+*/
+function Warrior(n){
+    let name = n;  // initially the variable hasn't been declared
+    this.name = function(n){
+        if( n ) name=n;
+        console.log(name)
+        return name;
+    }
+}
+Warrior.prototype.toString = function(){
+    return "Hi! my name's " + this.name;
+}
