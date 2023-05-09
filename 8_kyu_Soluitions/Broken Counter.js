4d43b5ca8552165a7d13d5d6a15926bf587ef6ff
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-05-09 14:47:29 +0700

    broken counter

---
+++ 8_kyu_Soluitions/Broken Counter.js
@@ -0,0 +1,23 @@
+/*Broken Counter
+Our counter prototype is broken. Can you spot, what's wrong here?
+*/
+
+function Counter() {
+        this.value = 0;
+    }
+    // to make everything function correct, we need to change () for = function() 
+    Counter.prototype.increase = function() {
+        this.value++;
+    };
+    //added this up, though it's not needed to pass tests
+    Counter.prototype.decrease = function() {
+        this.value--;
+    };
+
+    Counter.prototype.getValue = function() {
+        return this.value;
+    };
+
+    Counter.prototype.reset = function() {
+        this.value = 0;
+};
