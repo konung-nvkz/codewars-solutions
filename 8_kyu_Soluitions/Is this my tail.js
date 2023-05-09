cbec392f1225849c30cd959ede599706709ec980
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-05-09 13:38:40 +0700

    tail

---
+++ 8_kyu_Soluitions/Is this my tail.js
@@ -0,0 +1,25 @@
+/*Is this my tail?Is this my tail?
+Some new animals have arrived at the zoo. 
+The zoo keeper is concernnnot find name 'boolea'. Did you mean 'boolean'?ed that perhaps the animals do not have the right tails.
+To help her, you must correct the broken function to make sure that the second argument 
+(tail), is the same as the last letter of the first argument (body) - 
+otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+The arguments will always be non empty strings, and normal letters.
+*/
+
+function correctTail(body, tail) {  
+    let sub = body.substr(body.length-tail.length);
+    
+    if (sub == tail) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+//nothing scientific here. Just indentation, 
+//assignment instead of equal sign,
+//wrong kind of brackets and missed dots
