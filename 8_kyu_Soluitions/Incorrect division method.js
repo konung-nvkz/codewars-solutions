d2c99dc8cbc58db8fe09f3a798205844e7f22da1
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-05-12 10:38:17 +0700

    incorrect division

---
+++ 8_kyu_Soluitions/Incorrect division method.js
@@ -0,0 +1,8 @@
+/*Incorrect division method
+This method, which is supposed to return the result of dividing its first argument 
+by its second, isn't always returning correct values. Fix it.*/
+
+const solve = (x, y) => x / y
+
+// instead of // we need to leave / only. 
+// we don't need to comment, we need to divide, right?
