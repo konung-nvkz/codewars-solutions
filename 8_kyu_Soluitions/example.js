7d41507d3d476cad5f7e8a8ff56257cc91ba6546
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-06-26 14:15:00 +0700

    addition

---
+++ 8_kyu_Soluitions/example.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let options = {
+  title: "Меню",
+  width: 100,
+  height: 200
+};
+
+let {title, width, height} = options;
+
+console.log(title);  // Меню
+console.log(width);  // 100
+console.log(height); // 200
