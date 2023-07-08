4060aa038d74fe43fe42cfb77ff2d22113265a97
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-07-08 20:05:20 +0700

    parse integers

---
+++ 7_kyu_Solutions/[JS] Parse integers in array.js
@@ -0,0 +1,25 @@
+/*[JS] Parse integers in array
+DESCRIPTION:
+A colleague asked if you can help him fix his function. It seems it doesn't always parse Integers correctly.
+
+Examples:
+
+parseNumbers(['10']) // should return [10]
+parseNumbers(['-1','0','1']) // should return [-1,0,1]
+*/
+/*
+ * Function converts array of string to array of integers.
+ * @param  {Array} of numbers in form of string
+ * @return {Array} of integer numbers 
+ */
+var parseNumbers = function(intStrs) {
+    return intStrs.map(x => parseInt(x));
+}
+//one more variant with function declaration
+var parseNumbers = function(intStrs) {
+    return intStrs.map(
+        function(x) {
+            return parseInt(x);
+        }
+    );
+}
