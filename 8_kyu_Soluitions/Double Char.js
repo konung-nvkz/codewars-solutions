9beca9a99aa8d8fc41a61695336db090e7881a61
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-08-20 11:16:05 +0700

    doublechar

---
+++ 8_kyu_Soluitions/Double Char.js
@@ -0,0 +1,21 @@
+/*Double Char
+DESCRIPTION:
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
+*/
+function doubleChar(str) {    
+    let arr = str.split('');
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i] + arr[i]);
+    }
+    return result.join('');
+}
+
+console.log(doubleChar("String"));
+
