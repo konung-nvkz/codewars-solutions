fa30aa781b1beef4d2738045d3c0f7e240eb1f55
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-09-14 17:23:16 +0700

    reverser

---
+++ 7_kyu_Solutions/Reverser.js
@@ -0,0 +1,38 @@
+/*Reverser
+Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. You should do this without converting the inputted number into a string.
+
+// Please do not use
+const forbidden = "
+                    '
+                    `
+                    string
+                    fixed
+                    precision
+                    .keys
+*/
+//Mathematical way of solving the task
+// we take the task into steps:
+// first we take the result as 0остаток от делениядробная часть
+// at each iteration we multiply the result by 10 to move the digit to the left
+// and add the digit from the remainder of the division by 10
+// and change the number to the number/10 minus fraction
+// so for 23456 it will look like:
+// step one: reversed = 6, n = 2345
+// step two: reversed = 65, n = 234
+// step three: reversed = 654, n = 23
+// step foor: reversed = 6543, n = 2
+// step five: reversed = 65432, n = 0
+// at step 5 we are done
+
+function reverse(n){
+    let reversed = 0;
+    while (n !== 0) {
+        reversed = (reversed * 10) + (n % 10);
+        console.log(reversed);
+        n = Math.floor(n / 10);
+        console.log(n);
+    }
+    return reversed;
+}
+
+console.log(reverse(23456));
