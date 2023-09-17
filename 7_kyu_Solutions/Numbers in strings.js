193682dd528d5e969e7523388561818b91a5ae24
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-09-17 11:10:22 +0700

    numbers in strings

--- 7_kyu_Solutions/Numbers in strings.js
+++ 7_kyu_Solutions/Numbers in strings.js
@@ -1,6 +1,8 @@
 /*Numbers in strings
 DESCRIPTION:
-In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+In this Kata, you will be given a string that has lowercase letters and numbers. 
+Your task is to compare the number groupings and return the largest number. 
+Numbers will not have leading zeros.
 
 For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
 
@@ -23,3 +25,6 @@ function solve(str) {
 };
 
 console.log(solve("gh12cdy695m1"));
+
+//a solution with match
+//const solve = s => Math.max(...s.match(/\d+/g).map(Number));
