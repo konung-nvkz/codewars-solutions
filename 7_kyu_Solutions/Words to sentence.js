9881ca4e05ddca55add90bacf41a992dc49e14bb
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-11-04 15:20:48 +0700

    add words to sentemce

---
+++ 7_kyu_Solutions/Words to sentence.js
@@ -0,0 +1,13 @@
+/*Words to sentence
+DESCRIPTION:
+Write function which will create a string from a list of strings, 
+separated by space.
+
+Example:
+
+["hello", "world"] -> "hello world"
+*/
+// The task represents classic example of "join" string method.
+function wordsToSentence(words) {
+    return words.join(' ');
+}
