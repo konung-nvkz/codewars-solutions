374e12d19c6a9a085a3a88d7d8f128ae588d0656
Egor <117963845+konung-nvkz@users.noreply.github.com>
2023-11-11 18:51:21 +0700

    some debugging

---
+++ 7_kyu_Solutions/More than Zero.py
@@ -0,0 +1,10 @@
+#  More than Zero?
+# DESCRIPTION:
+# Correct this code so that it takes one argument, x, and returns "x is more than zero" if x is positive (and nonzero), and otherwise, returns "x is equal to or less than zero." 
+# In both cases, replace x with the actual value of x.
+
+def corrections(x):
+    if x > 0:
+        return str(x) + " is more than zero."
+    else:
+        return str(x) + " is equal to or less than zero."
