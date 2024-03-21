8a3898cd1f3af2cb72b6d7748543f3e5faff0a9f
Egor <117963845+konung-nvkz@users.noreply.github.com>
2024-03-21 10:08:16 +0700

    add chinese zodiac

---
+++ 7_kyu_Solutions/Chinese Zodiac.js
@@ -0,0 +1,35 @@
+/*Chinese Zodiac
+
+The Chinese zodiac is a repeating cycle of 12 years, with each year being represented by an animal and its reputed attributes. The lunar calendar is divided into cycles of 60 years each, and each year has a combination of an animal and an element. There are 12 animals and 5 elements; the animals change each year, and the elements change every 2 years. The current cycle was initiated in the year of 1984 which was the year of the Wood Rat.
+
+Since the current calendar is Gregorian, I will only be using years from the epoch 1924. For simplicity I am counting the year as a whole year and not from January/February to the end of the year.
+
+##Task
+
+Given a year, return the element and animal that year represents ("Element Animal"). For example I'm born in 1998 so I'm an "Earth Tiger".
+
+animals (or $animals in Ruby) is a preloaded array containing the animals in order:
+
+['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Goat', 'Monkey', 'Rooster', 'Dog', 'Pig']
+
+elements (or $elements in Ruby) is a preloaded array containing the elements in order:
+
+['Wood', 'Fire', 'Earth', 'Metal', 'Water']
+
+Tell me your zodiac sign and element in the comments. Happy coding :)
+
+*/
+
+function chineseZodiac(year) {
+    const animals = ['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Goat', 'Monkey', 'Rooster', 'Dog', 'Pig'];
+    const elements = ['Wood', 'Fire', 'Earth', 'Metal', 'Water'];
+    let animal = animals[(year - 1924) % 12];
+    let element = elements[(Math.floor((year - 1924) / 2)) % 5];
+    return `${element} ${animal}`;
+}
+
+console.log(chineseZodiac(1925));//Wood Ox
+console.log(chineseZodiac(1930));//Metal Horse
+console.log(chineseZodiac(1956));//Fire Monkey
+console.log(chineseZodiac(2012)); //Water Dragon
+console.log(chineseZodiac(2024)); //Wood Fragon
