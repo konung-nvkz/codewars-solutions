7566d6dcabb356f0a082ca08f4a7aff02e622b40
Egor <117963845+konung-nvkz@users.noreply.github.com>
2024-05-18 08:46:29 +0700

    add sum of the strings

---
+++ 8_kyu_Solutions/Sum The Strings.js
@@ -0,0 +1,32 @@
+/*Sum The Strings
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+Notes:
+
+If either input is an empty string, consider it as zero.
+
+Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+*/
+
+function sumStr(a,b) {
+    return (a=="")&&(b=="") ? "0" :
+    (a!=="")&&(b=="") ? a :
+    (a=="")&&(b!=="") ? b :
+    (parseInt(a, 10) + parseInt(b, 10)).toString();
+}
+
+//Tests
+
+console.log(sumStr("4","5"));//, "9");
+console.log(sumStr("34","5"));//, "39");
+
+function sumStr(a,b) {
+    return String(Number(a)+Number(b));
+}
