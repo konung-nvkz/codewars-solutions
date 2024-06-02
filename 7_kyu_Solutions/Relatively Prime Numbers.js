91d272654f331e7b6d9a0c9baa29dd1d792353ad
Egor <117963845+konung-nvkz@users.noreply.github.com>
2024-06-02 21:50:05 +0700

    change tabulation

--- 7_kyu_Solutions/Relatively Prime Numbers.js
+++ 7_kyu_Solutions/Relatively Prime Numbers.js
@@ -64,12 +64,10 @@ function relativelyPrime(n, arr) {
     return result;
 }
 
-    //Tests
-    console.log(relatively_prime(8, [1, 2, 3, 4, 5, 6, 7]));  // Output: [1, 3, 5, 7]
-    
-    console.log(relatively_prime(15, [72, 27, 32, 61, 77, 11, 40]));   // Output: [32, 61, 77, 11]
-    
-    console.log(relatively_prime(210, [15, 100, 2222222, 6, 4, 12369, 99]));   // Output: [15, 100, 2222222, 6, 4, 12369, 99]
+//Tests
+console.log(relatively_prime(8, [1, 2, 3, 4, 5, 6, 7]));  // Output: [1, 3, 5, 7]
+console.log(relatively_prime(15, [72, 27, 32, 61, 77, 11, 40]));   // Output: [32, 61, 77, 11]
+console.log(relatively_prime(210, [15, 100, 2222222, 6, 4, 12369, 99]));   // Output: [15, 100, 2222222, 6, 4, 12369, 99]
 
 //     The Euclidean Algorithm to Calculate the GCF
 
