30a69a76a9cfda0977c62ae1687d5b1a176a3086
Egor <117963845+konung-nvkz@users.noreply.github.com>
2024-07-29 07:06:04 +0700

    add weekdays

---
+++ 8_kyu_Solutions/Return the day.js
@@ -0,0 +1,22 @@
+/* Return the day
+Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"
+*/
+
+function whatday(num) {
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    if (num >= 1 && num <= 7) {
+        return weekDays[num - 1];
+    } else {
+        return "Wrong, please enter a number between 1 and 7";
+    }
+}
