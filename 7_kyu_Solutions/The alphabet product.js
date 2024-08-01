847c03110995d8dde771fbf2b98d6c2b2bcb698e
Egor <117963845+konung-nvkz@users.noreply.github.com>
2024-08-01 07:18:58 +0700

    add solutio

--- 7_kyu_Solutions/The alphabet product.js
+++ 7_kyu_Solutions/The alphabet product.js
@@ -11,3 +11,18 @@ function alphabet(ns) {
 console.log(alphabet([2,3,4,1,12,6,2,4]))//, 4 );)
 
 console.log(alphabet([2,6,7,3,14,35,15,5]))//, 7 );
+
+return sorted[7] / sorted[sorted[0] * sorted[1] == sorted[2] ? 3 : 2]
+
+function alphabet(ns) {
+    const arr = ns.sort((a, b) => a - b);
+    // Delete AxB
+    let ab = arr.find((e) => e === arr[0] * arr[1]);
+    arr.splice(arr.indexOf(ab), 1);
+    // A and B are first two numbers, CxD is the greatest. Delete A, B and CxD
+    let newArr = arr.slice(2, 6);
+    //now we have  C , D, BxC DxA. C is smallest
+    let c = newArr[0];
+    //return to first array and find D from CxD (the biggest -the last one)
+    return arr[6] / c;
+}
