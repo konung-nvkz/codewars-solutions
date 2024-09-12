788605180e1e6a66198f82565bda997d2ce2c697
Egor <117963845+konung-nvkz@users.noreply.github.com>
2024-09-12 22:10:23 +0700

    old to new

--- 8_kyu_Solutions/Switch it Up!.js
+++ 8_kyu_Solutions/Switch it Up!.js
@@ -24,3 +24,41 @@ function switchItUp(number){
     };
     return units[number];
 }
+
+//Switch/case
+function switchItUp(number) {
+    switch (number) {
+        case 0:
+            return "Zero";
+        case 1:
+            return "One";
+        case 2:
+            return "Two";
+        case 3:
+            return "Three";
+        case 4:
+            return "Four";
+        case 5:
+            return "Five";
+        case 6:
+            return "Six";
+        case 7:
+            return "Seven";
+        case 8:
+            return "Eight";
+        case 9:
+            return "Nine";
+        default:
+            return "Invalid input";
+        }
+    }
+
+//One more Variant, Using an Array Lookup
+
+function switchItUp(number) {
+    const units = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+    if (number < 0 || number > 9) {
+        return "Invalid input";
+    }
+    return units[number];
+}
