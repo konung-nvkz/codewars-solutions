be5c42cb4578f26e2340a90d07f02109943e09bd
Egor <117963845+konung-nvkz@users.noreply.github.com>
2024-09-13 09:22:40 +0700

    put python code to comments

--- 8_kyu_Solutions/Super Duper Easy.js
+++ 8_kyu_Solutions/Super Duper Easy.js
@@ -12,12 +12,12 @@ console.log(problem(5)); // 256
 console.log(problem("hello")); // "error"
 
 
-//And here's the Python code:
-
+//And here's the Python code. I put it inside comments during review. Need to be put to the other file to compile.
+/*
 def problem(value):
     if isinstance(value, str):
         return "Error"
     return value * 50 + 6;
 
 print(problem(5)) # 256
-print(problem("hello")) # "error
+print(problem("hello")) # "error*/
